8fc4506851ffaf65189a87a4cd6cdaa82f68cc20
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-10-17 09:45:12 +0200

    added banner section and adding compare to others section now aswell as a price section

---
+++ src/app/_components/Forfun.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+import { set } from "zod";
+
+type Props = {};
+
+function Forfun({}: Props) {
+  const [counter, setCounter] = useState(0);
+
+  const increCount = (counter: number) => {
+    setCounter(counter + 1);
+  };
+
+  const decremeant = (counter: number) => {
+    if (counter >= 1) {
+      setCounter(counter - 1);
+    }
+    return null;
+  };
+  return (
+    <div className="flex items-center p-15">
+      <h1>zoooo</h1>
+      <button
+        className="cursor-pointer border-4 bg-red-400"
+        onClick={(e) => {
+          increCount(counter);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={(e) => {
+          decremeant(counter);
+        }}
+        className="cursor-pointer border-4 bg-green-400"
+      >
+        -
+      </button>
+      <p>Current Count: {counter}</p>
+      <button
+        onClick={(e) => {
+          setCounter(0);
+        }}
+        className="cursor-pointer border-4 bg-yellow-400"
+      >
+        Clear
+      </button>
+    </div>
+  );
+}
+
+export default Forfun;
