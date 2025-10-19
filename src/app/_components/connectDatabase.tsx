3a1dc79c4dd3e72794b697aeec75c5caa25472b3
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-10-19 15:52:08 +0200

    fix deploy

--- src/app/_components/connectDatabase.tsx
+++ src/app/_components/connectDatabase.tsx
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://yoqaywxcpimzcawxyade.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY || "";
+if (!supabaseKey) {
+  throw new Error("SUPABASE_KEY is not defined in the environment variables.");
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
