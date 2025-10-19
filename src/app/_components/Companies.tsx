9de7ce5b242618d73d34f0ad327b980b7c13aff5
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2025-10-19 15:39:45 +0200

    update images added a footer making rdy for frontend deployment

--- src/app/_components/Companies.tsx
+++ src/app/_components/Companies.tsx
@@ -1,9 +1,62 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Companies({}: Props) {
+  const arrowContentCompanies = [
+    {
+      arrow: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          style={{ fill: "#FD7E14" }}
+        >
+          <path d="M 20.738281 5.9941406 A 1.250125 1.250125 0 0 0 19.878906 6.3730469 L 9 17.234375 L 4.1152344 12.361328 A 1.250125 1.250125 0 1 0 2.3496094 14.130859 L 8.1171875 19.884766 A 1.250125 1.250125 0 0 0 9.8828125 19.884766 L 21.644531 8.140625 A 1.250125 1.250125 0 0 0 20.738281 5.9941406 z"></path>
+        </svg>
+      ),
+      line: "Schnell & Einfach",
+    },
+    {
+      arrow: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          style={{ fill: "#FD7E14" }}
+        >
+          <path d="M 20.738281 5.9941406 A 1.250125 1.250125 0 0 0 19.878906 6.3730469 L 9 17.234375 L 4.1152344 12.361328 A 1.250125 1.250125 0 1 0 2.3496094 14.130859 L 8.1171875 19.884766 A 1.250125 1.250125 0 0 0 9.8828125 19.884766 L 21.644531 8.140625 A 1.250125 1.250125 0 0 0 20.738281 5.9941406 z"></path>
+        </svg>
+      ),
+      line: "Tests für alle Firmen",
+    },
+    {
+      arrow: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          style={{ fill: "#FD7E14" }}
+        >
+          <path d="M 20.738281 5.9941406 A 1.250125 1.250125 0 0 0 19.878906 6.3730469 L 9 17.234375 L 4.1152344 12.361328 A 1.250125 1.250125 0 1 0 2.3496094 14.130859 L 8.1171875 19.884766 A 1.250125 1.250125 0 0 0 9.8828125 19.884766 L 21.644531 8.140625 A 1.250125 1.250125 0 0 0 20.738281 5.9941406 z"></path>
+        </svg>
+      ),
+      line: "Individuelles Feedback",
+    },
+  ];
   const logos = [
     {
       name: "VW",
@@ -13,16 +66,28 @@ function Companies({}: Props) {
     { name: "Telekom", src: "/telekom.png" },
     { name: "Rossmann", src: "/rossmann.png" },
     { name: "HDI", src: "/hdi.png" },
+    { name: "Bundeswehr", src: "/Bundeswehr.png" },
+    { name: "DB", src: "/DB.png" },
+    { name: "Siemens", src: "/Siemens.png" },
   ];
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 bg-gradient-to-br from-[#FF705B]/10 to-[#FFB457]/10 py-16">
-      <h1 className="mb-4 text-3xl font-bold sm:text-4xl">
-        Übe für Firmen Einstellungstests
-      </h1>
-      <p className="text-lg">übe speziell nach Beruf und Firma</p>
-      <div className="flex flex-row space-x-7">
+    <div className="flex flex-col items-center justify-center space-y-8 rounded-2xl bg-gradient-to-br from-[#FF705B]/10 to-[#FFB457]/10 py-16 shadow-2xl">
+      <div className="sapce-y-2 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
+          Übe für Firmen Einstellungstests
+        </h1>
+        <p className="text-sm text-black/90">
+          übe speziell nach Beruf und Firma
+        </p>
+      </div>
+
+      {/* Logos */}
+      <div className="animate-scroll mt-6 flex flex-row flex-wrap justify-center gap-8">
         {logos.map((logo, i) => (
-          <div key={i} className="relative h-16 w-32">
+          <div
+            key={i}
+            className="relative h-16 w-32 p-2 backdrop-blur-md transition-transform duration-300 hover:scale-105"
+          >
             <Image
               src={logo.src}
               alt={logo.name}
@@ -31,6 +96,22 @@ function Companies({}: Props) {
               style={{ objectFit: "contain" }}
             />
           </div>
+        ))}
+      </div>
+
+      {/* Arrow Features */}
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        {arrowContentCompanies.map((arrow, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: -44 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7, delay: index * 0.1 }}
+            className="flex items-center gap-2 rounded-2xl bg-[#2B2B3C]/80 p-4 shadow-lg backdrop-blur-md transition-discrete hover:scale-105 hover:border-[#FF705B] hover:shadow-xl"
+          >
+            {arrow.arrow}
+            <p className="text-sm font-semibold text-white">{arrow.line}</p>
+          </motion.div>
         ))}
       </div>
     </div>
--- src/app/_components/Price.tsx
+++ src/app/_components/Price.tsx
@@ -1,9 +1,91 @@
 import React from "react";
 
 type Props = {};
+// price winner color bg-gradient-to-br from-[#FF705B]/10 to-[#FFB457]/10
+// price outside bg-[#2B2B3C]
 
 function Price({}: Props) {
-  return <div>Price</div>;
+  return (
+    <div className="flex flex-col justify-center">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
+          {" "}
+          Waehle deinen Plan
+        </h1>
+        <p>
+          Starte kostenlos und aboniere sobald du auf das neachste lvl kommen
+          moechtest
+        </p>
+      </div>
+
+      {/*price plans kostenlos pro und premoum */}
+
+      <div className="mt-8 flex flex-col space-y-8 md:flex-row md:justify-center md:space-y-0 md:space-x-8">
+        {/* Kostenlos Plan */}
+        <div className="flex flex-col rounded-2xl border border-[#FF705B] bg-[#2B2B3C] p-6 shadow-xl backdrop-blur-md hover:brightness-105 md:w-1/4">
+          <h2 className="mb-4 text-2xl font-bold text-white">Kostenlos</h2>
+          <p className="mb-6 text-white/70">Ideal zum Ausprobieren</p>
+          <div className="mb-6">
+            <span className="text-4xl font-extrabold text-white">0€</span>
+            <span className="text-white/70"> / Monat</span>
+          </div>
+          <ul className="mb-6 space-y-4 text-white/70">
+            <li>🔹 Zugang zu Basisfragen</li>
+            <li>🔹 5 Tests pro Monat</li>
+            <li>🔹 Grundlegende Auswertung</li>
+          </ul>
+          <button className="bg-base-300 mt-auto cursor-pointer rounded-full px-6 py-3 font-semibold text-white hover:brightness-105">
+            Loslegen
+          </button>
+        </div>
+
+        {/* Pro Plan */}
+        <div className="relative flex scale-105 flex-col rounded-2xl border-4 border-[#FFB457] bg-[#2B2B3C] p-6 shadow-xl backdrop-blur-md hover:brightness-105 md:w-1/4">
+          {/* just learned that u can move the actual starting point with left-1/2 and then center it with translate x*/}
+          <div className="absolute -top-3 left-1/2 -translate-x-1/2">
+            <span className="rounded-full bg-gradient-to-br from-[#FF705B] to-[#FFB457] px-4 py-1 text-sm font-semibold text-white shadow-lg">
+              Am häufigsten gekauft
+            </span>
+          </div>
+
+          <h2 className="mb-4 text-2xl font-bold text-white">Pro</h2>
+          <p className="mb-6 text-white/70">Für ambitionierte Lerner</p>
+          <div className="mb-6">
+            <span className="text-4xl font-extrabold text-white">9.99€</span>
+            <span className="text-white/70"> / Monat</span>
+          </div>
+          <ul className="mb-6 space-y-4 text-white/70">
+            <li>🔹 Zugang zu allen automatisch generierten Tests</li>
+            <li>🔹 Unbegrenzte Testversuche</li>
+            <li>🔹 Sofortige Auswertung & Ergebnisse</li>
+            <li>🔹 Test-Schwierigkeit anpassbar</li>
+          </ul>
+          <button className="mt-auto cursor-pointer rounded-full bg-gradient-to-br from-[#FF705B] to-[#FFB457] px-6 py-3 font-semibold text-white hover:brightness-105">
+            Jetzt Abonnieren
+          </button>
+        </div>
+
+        {/* Premium Plan */}
+        <div className="flex flex-col rounded-2xl border border-[#FF705B] bg-[#2B2B3C] p-6 shadow-xl backdrop-blur-md hover:brightness-105 md:w-1/4">
+          <h2 className="mb-4 text-2xl font-bold text-white">Premium</h2>
+          <p className="mb-6 text-white/70">Für Profis und Teams</p>
+          <div className="mb-6">
+            <span className="text-4xl font-extrabold text-white">19.99€</span>
+            <span className="text-white/70"> / Monat</span>
+          </div>
+          <ul className="mb-6 space-y-4 text-white/70">
+            <li>🔹 Alle Pro-Funktionen</li>
+            <li>🔹 Tests für mehrere Nutzer gleichzeitig</li>
+            <li>🔹 Erweiterte Analyse & Statistiken</li>
+            <li>🔹 Exportfunktion für Tests und Ergebnisse</li>
+          </ul>
+          <button className="mt-auto cursor-pointer rounded-full bg-gradient-to-br from-[#FF705B] to-[#FFB457] px-6 py-3 font-semibold text-white hover:brightness-105">
+            Jetzt Abonnieren
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Price;
