2526215dd6d72496611126a9e3f5c0120f1dcbda
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-11-20 19:29:13 +0100

    added TODO

--- src/app/PremiumUsers/page.tsx
+++ src/app/PremiumUsers/page.tsx
@@ -5,7 +5,8 @@ type Props = {};
 
 export default function page({}: Props) {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center gap-10 py-10">
+      <h1 className="text-7xl">Premium </h1>
       <Get_ML_Model_Result />
     </div>
   );
---
+++ src/app/_components/PremiumDahsboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+//::TODO: create modern Dashboard
+
+function PremiumDahsboard({}: Props) {
+  return (
+    <div className="flex">
+      <h1>Overview</h1>
+    </div>
+  );
+}
+
+export default PremiumDahsboard;
