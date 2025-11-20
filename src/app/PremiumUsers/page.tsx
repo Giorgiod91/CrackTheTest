b6c5f23d6e7c5ebe641660e701bd17b3fd48628f
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-11-20 18:55:30 +0100

    creatine new pages for premium users

---
+++ src/app/PremiumUsers/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Get_ML_Model_Result from "../_components/Get_ML_Model_Result";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <Get_ML_Model_Result />
+    </div>
+  );
+}
