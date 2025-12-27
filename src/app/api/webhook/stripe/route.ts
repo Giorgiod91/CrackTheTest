32e0edb874912df4c4cad380545441479f6ae20c
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-12-27 16:45:49 +0100

    deploy

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -10,7 +10,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
 );
-
+//////
 type UserRecord = {
   email: string;
   real_member_id: string | null;
