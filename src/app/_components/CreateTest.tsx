19c4c8de7b3bb11a582d343be8698a31e379b2e2
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2026-01-01 20:10:13 +0100

    added todo

--- src/app/_components/CreateTest.tsx
+++ src/app/_components/CreateTest.tsx
@@ -6,6 +6,11 @@ function CreateTest() {
   // for later user
   const [subject, setSubject] = useState("");
   const [content, setContent] = useState("");
+  
+
+  // normalize input to see if some users having same input so I dont have to recall openai API and can just
+  // give out the same Test and reduce the api token cost
+  //::TODO: add hashing , 
 
   async function pushTests(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -17,11 +22,13 @@ function CreateTest() {
     }
 
     // API call to OpenAI
-    const response = await client.responses.create({
-            model: "gpt-5-nano",
-            input:  `Create a full realistic Test for the users need like a real company Einstellungstest for the topic${title}
-            and content should be about${content} and all of this should prepare the user to work at the company who gives out tests like those`
-    })
+const response = await openai.responses.create({
+    model: "gpt-5-nano",
+    input: `Create a realistic company-style test for:
+    Title: ${input.title}
+    Subject: ${input.subject}
+    Content: ${input.content}`,
+  });
 
      // saving the test here
     let test = resonse.output_text;
